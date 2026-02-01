8ade77bd8b6457d565937d87d5a8d5f912b6ded0
mstange98 <116954176+mstange98@users.noreply.github.com>
2026-02-01 14:26:54 +0100

    Update config.js

--- config.js
+++ config.js
@@ -31,6 +31,7 @@ const CONFIG = {
             text: "How much do you love me?",                          // For the love meter
             startText: "This much!",                                   // Text before the percentage
             nextBtn: "Next ❤️"                                         // Text for the next button
+            
         },
         third: {
             text: "Will you be my Valentine on February 14th, 2026? 🌹", // The big question!
@@ -42,16 +43,16 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
+        extreme: "WOW you love me so much that it seems that you would like to have me as your valentine🚀💝 ",  // Shows when they go past 5000%
         high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
         normal: "And beyond! 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
+        title: "Yay! I'm the luckiest person in the world! 🎉💝",
         message: "Now come get your gift, a big warm hug and a huge kiss!",
-        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
+        emojis: "🎁❤️"  // These will bounce around
     },
 
     // Color scheme for the website
